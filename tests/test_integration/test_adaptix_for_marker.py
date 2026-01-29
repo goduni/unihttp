1b260e9f5265dc0aa72bfc5c09d7951187503cee
K1rL3s <kirill.lesovoi@mail.ru>
2026-01-29 08:10:20 +0300

    feat: Improve coverage of the 'for_marker'

--- tests/test_integration/test_adaptix_for_marker.py
+++ tests/test_integration/test_adaptix_for_marker.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any
+from typing import Annotated, Any
 
 from adaptix import Omittable, Omitted, P, dumper
 from unihttp.markers import (
     Body,
     BodyMarker,
+    Form,
+    FormMarker,
     Header,
     HeaderMarker,
     Path,
@@ -19,9 +21,14 @@ from unihttp.serializers.adaptix.marker_tools import for_marker
 
 
 def test_for_marker() -> None:
-    @dataclass
+    class SubHeaderMarker(HeaderMarker):
+        pass
+
+    @dataclass(kw_only=True)
     class MyData(BaseMethod[Any]):
         header_dt: Header[datetime]
+        header_root_str: Header[str]
+        header_sub_str: Annotated[str, SubHeaderMarker()]
         path_str: Path[str | None]
         path_none: Path[str | None]
         query_int: Query[list[int]]
@@ -31,6 +38,8 @@ def test_for_marker() -> None:
         body_default_none: Body[Any] = None
         body_omitted: Body[Omittable[str]] = Omitted()
         body_filled: Body[Omittable[str]] = Omitted()
+        form_anyway: Form[str]
+        random_field: str
 
     retort = DEFAULT_RETORT.extend(
         recipe=[
@@ -50,11 +59,21 @@ def test_for_marker() -> None:
                 for_marker(BodyMarker, P[int]),
                 lambda t: datetime.fromtimestamp(t),
             ),
+            dumper(
+                for_marker(HeaderMarker, P[str], subclass=True),
+                lambda x: int(x),
+            ),
+            dumper(
+                for_marker(FormMarker),
+                lambda x: int(x),
+            ),
         ]
     )
 
     data = MyData(
         header_dt=datetime.fromtimestamp(1234567890),
+        header_root_str="123",
+        header_sub_str="1234",
         path_str="pathlike",
         path_none=None,
         query_int=[1, 2, 3],
@@ -64,13 +83,23 @@ def test_for_marker() -> None:
         # `body_default_none` acting like `Omitted` when `= None` idk why
         # `body_omitted` is just `Omitted`
         body_filled="filled",
+        form_anyway="12345",
+        random_field="random",
     )
     excepted = {
         "header": {
             "header_dt": 1234567890.0,
+            "header_root_str": 123,
+            "header_sub_str": 1234,
         },
-        "path": {"path_str": "pathlike", "path_none": "null"},
-        "query": {"query_int": "1,2,3", "query_str": "4,5,6"},
+        "path": {
+            "path_str": "pathlike",
+            "path_none": "null",
+        },
+        "query": {
+            "query_int": "1,2,3",
+            "query_str": "4,5,6",
+        },
         "body": {
             "body_int": datetime.fromtimestamp(1234567890),
             "body_none": None,
@@ -78,5 +107,9 @@ def test_for_marker() -> None:
             # `body_omitted` is just `Omitted`
             "body_filled": "filled",
         },
+        "form": {
+            "form_anyway": 12345,
+        },
+        "random_field": "random",
     }
     assert retort.dump(data) == excepted
