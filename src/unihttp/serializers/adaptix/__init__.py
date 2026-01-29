3517c9ee5f7bf6bd750dec161feb69bea0ada8e4
K1rL3s <kirill.lesovoi@mail.ru>
2026-01-29 09:02:45 +0300

    feat: Add `for_marker` to `serializers.adaptix.__init__.py`

--- src/unihttp/serializers/adaptix/__init__.py
+++ src/unihttp/serializers/adaptix/__init__.py
@@ -1,6 +1,7 @@
 from .fixed_tp_tags_unwrapping import fixed_type_hint_tags_unwrapping_provider
 from .provider import method_provider
 from .serialize import DEFAULT_RETORT, AdaptixDumper, AdaptixLoader
+from .marker_tools import for_marker
 
 __all__ = [
     "DEFAULT_RETORT",
@@ -8,4 +9,5 @@ __all__ = [
     "AdaptixLoader",
     "fixed_type_hint_tags_unwrapping_provider",
     "method_provider",
+    "for_marker",
 ]
