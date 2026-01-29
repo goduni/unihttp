fb4a7ae69060cc8ef409203ec7fff03f4638e225
K1rL3s <kirill.lesovoi@mail.ru>
2026-01-29 09:18:21 +0300

    feat: Fix Ruff errors

--- src/unihttp/serializers/adaptix/__init__.py
+++ src/unihttp/serializers/adaptix/__init__.py
@@ -1,13 +1,13 @@
 from .fixed_tp_tags_unwrapping import fixed_type_hint_tags_unwrapping_provider
+from .marker_tools import for_marker
 from .provider import method_provider
 from .serialize import DEFAULT_RETORT, AdaptixDumper, AdaptixLoader
-from .marker_tools import for_marker
 
 __all__ = [
     "DEFAULT_RETORT",
     "AdaptixDumper",
     "AdaptixLoader",
     "fixed_type_hint_tags_unwrapping_provider",
-    "method_provider",
     "for_marker",
+    "method_provider",
 ]
